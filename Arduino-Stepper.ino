81d19342a4d15c9a40ab6e5661916eaeb468a0bd
Abner2111 <abneraq73@gmail.com>
2024-05-16 23:16:29 -0600

    test with 3 motors

--- Arduino-Stepper.ino
+++ Arduino-Stepper.ino
@@ -1,88 +1,131 @@
-int IN1 = 8;
-int IN2 = 9;
-int IN3 = 10;
-int IN4 = 11;
+int IN1z = 13;
+int IN2z = 12;
+int IN3z = 11;
+int IN4z = 10;
+
+int IN1y = 9;
+int IN2y = 8;
+int IN3y = 7;
+int IN4y = 6;
+
+int IN1x = 5;
+int IN2x = 4;
+int IN3x = 3;
+int IN4x = 2;
 const int stepsPerRotation = 512;
 const int stepperdelay = 1000;
 
 
-void write(int a, int b, int c, int d){
-  digitalWrite(IN1,a);
-  digitalWrite(IN2,b);
-  digitalWrite(IN3,c);
-  digitalWrite(IN4,d);
+void write(int a, int b, int c, int d, int coord){
+  if (coord == 0){
+    digitalWrite(IN1x,a);
+    digitalWrite(IN2x,b);
+    digitalWrite(IN3x,c);
+    digitalWrite(IN4x,d);
+  } else if (coord == 1) {
+    digitalWrite(IN1y,a);
+    digitalWrite(IN2y,b);
+    digitalWrite(IN3y,c);
+    digitalWrite(IN4y,d);
+  }
+  else if (coord == 2) {
+    digitalWrite(IN1z,a);
+    digitalWrite(IN2z,b);
+    digitalWrite(IN3z,c);
+    digitalWrite(IN4z,d);
+  }
+  
 }
 
 //CW step
-void onestep(){
-  write(1,0,0,0);
+void onestep(int coord){
+  write(1,0,0,0, coord);
   delayMicroseconds(stepperdelay);
-  write(1,1,0,0);
+  write(1,1,0,0, coord);
   delayMicroseconds(stepperdelay);
-  write(0,1,0,0);
+  write(0,1,0,0, coord);
   delayMicroseconds(stepperdelay);
-  write(0,1,1,0);
+  write(0,1,1,0, coord);
   delayMicroseconds(stepperdelay);
-  write(0,0,1,0);
+  write(0,0,1,0, coord);
   delayMicroseconds(stepperdelay);
-  write(0,0,1,1);
+  write(0,0,1,1, coord);
   delayMicroseconds(stepperdelay);
-  write(0,0,0,1);
+  write(0,0,0,1, coord);
   delayMicroseconds(stepperdelay);
-  write(1,0,0,1);
+  write(1,0,0,1, coord);
   delayMicroseconds(stepperdelay);
 }
 
 //ACW step
-void negonestep(){
-  write(0,0,0,1);
+void negonestep(int coord){
+  write(0,0,0,1, coord);
   delayMicroseconds(stepperdelay);
-  write(0,0,1,1);
+  write(0,0,1,1, coord);
   delayMicroseconds(stepperdelay);
-  write(0,0,1,0);
+  write(0,0,1,0, coord);
   delayMicroseconds(stepperdelay);
-  write(0,1,1,0);
+  write(0,1,1,0, coord);
   delayMicroseconds(stepperdelay);
-  write(0,1,0,0);
+  write(0,1,0,0, coord);
   delayMicroseconds(stepperdelay);
-  write(1,1,0,0);
+  write(1,1,0,0, coord);
   delayMicroseconds(stepperdelay);
-  write(1,0,0,0);
+  write(1,0,0,0, coord);
   delayMicroseconds(stepperdelay);
-  write(1,0,0,1);
+  write(1,0,0,1, coord);
   delayMicroseconds(stepperdelay);
 }
 
 //CW turn
-void step(int number_of_steps){
+void step(int number_of_steps, int coord){
   int i;
   i=0;
   while(i<number_of_steps){
-    onestep();
+    onestep(coord);
     i++;
   }
 }
 
 //ACW turn
-void negstep(int number_of_steps){
+void negstep(int number_of_steps, int coord){
   int i;
   i=0;
   while(i<number_of_steps){
-    negonestep();
+    negonestep(coord);
     i++;
   }
 }
 void setup(){
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
+  pinMode(IN1x, OUTPUT);
+  pinMode(IN2x, OUTPUT);
+  pinMode(IN3x, OUTPUT);
+  pinMode(IN4x, OUTPUT);
+
+  pinMode(IN1y, OUTPUT);
+  pinMode(IN2y, OUTPUT);
+  pinMode(IN3y, OUTPUT);
+  pinMode(IN4y, OUTPUT);
+
+  pinMode(IN1z, OUTPUT);
+  pinMode(IN2z, OUTPUT);
+  pinMode(IN3z, OUTPUT);
+  pinMode(IN4z, OUTPUT);
+  delay(5000);
 }
 
 void loop(){
-  step(stepsPerRotation);
+  negstep(stepsPerRotation/4, 2);
+  step(stepsPerRotation/5, 2);
+  for (int i = 0; i<=stepsPerRotation*3.5;i++){
+      step(1, 0);
+      negstep(1, 1);
+  }
   delay(1000);
 
-  negstep(stepsPerRotation);
+  for (int i = 0; i<=stepsPerRotation*3.5;i++){
+      negstep(1, 0);
+      step(1, 1);
+  }
   delay(1000);
 }
