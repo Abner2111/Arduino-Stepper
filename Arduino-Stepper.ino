9c69e1fc6cca5e876fd5e6f4bcca75e95da39713
Abner2111 <abneraq73@gmail.com>
2024-05-17 18:24:40 -0600

    calibration mode

--- Arduino-Stepper.ino
+++ Arduino-Stepper.ino
@@ -12,10 +12,17 @@ int IN1x = 5;
 int IN2x = 4;
 int IN3x = 3;
 int IN4x = 2;
+
+int calibrated = 18;
+int right      = 19;
+int up         = 20;
+int left       = 21;
+int down       = 22;
+
 const int stepsPerRotation = 512;
 const int stepperdelay = 1000;
 
-
+int prevCalibrated = 0;
 void write(int a, int b, int c, int d, int coord){
   if (coord == 0){
     digitalWrite(IN1x,a);
@@ -55,6 +62,8 @@ void onestep(int coord){
   delayMicroseconds(stepperdelay);
   write(1,0,0,1, coord);
   delayMicroseconds(stepperdelay);
+  write(0,0,0,0, coord);
+  delayMicroseconds(stepperdelay);
 }
 
 //ACW step
@@ -74,6 +83,8 @@ void negonestep(int coord){
   write(1,0,0,0, coord);
   delayMicroseconds(stepperdelay);
   write(1,0,0,1, coord);
+  delayMicroseconds(stepperdelay);
+  write(0,0,0,0, coord);
   delayMicroseconds(stepperdelay);
 }
 
@@ -111,21 +122,57 @@ void setup(){
   pinMode(IN2z, OUTPUT);
   pinMode(IN3z, OUTPUT);
   pinMode(IN4z, OUTPUT);
-  delay(5000);
+  
+  pinMode(calibrated, OUTPUT);
+  pinMode(right, OUTPUT);
+  pinMode(left, OUTPUT);
+  pinMode(up, OUTPUT);
+  pinMode(down, OUTPUT);
+
+  delay(500);
 }
 
 void loop(){
-  negstep(stepsPerRotation/4, 2);
-  step(stepsPerRotation/5, 2);
-  for (int i = 0; i<=stepsPerRotation*3.5;i++){
-      step(1, 0);
-      negstep(1, 1);
+  if (prevCalibrated != digitalRead(calibrated)){
+    if(digitalRead(calibrated)){
+      negstep(stepsPerRotation/6, 2);
+      step(stepsPerRotation/6+5, 2);
+    }
+    prevCalibrated = digitalRead(calibrated);
   }
-  delay(1000);
+  else if(digitalRead(calibrated)){
+    
+    for (int i = 0; i<=stepsPerRotation*3.5;i++){
+        step(1, 0);
+        negstep(1, 1);
+        if (!digitalRead(calibrated)){
+          break;
+        }
+    }
+    delay(1000);
 
-  for (int i = 0; i<=stepsPerRotation*3.5;i++){
-      negstep(1, 0);
-      step(1, 1);
+    for (int i = 0; i<=stepsPerRotation*3.5;i++){
+        negstep(1, 0);
+        step(1, 1);
+        if (!digitalRead(calibrated)){
+          break;
+        }
+    }
+    delay(1000);
+  } else {
+    digitalWrite(IN1z,0);
+    digitalWrite(IN2z,0);
+    digitalWrite(IN3z,0);
+    digitalWrite(IN4z,0);
+    if(digitalRead(up)){
+      step(1,1);
+    } else if(digitalRead(down)) {
+      negstep(1,1);
+    } if(digitalRead(right)) {
+      step(1,0);
+    } else if(digitalRead(left)) {
+      negstep(1,0);
+    }
   }
-  delay(1000);
+  
 }
